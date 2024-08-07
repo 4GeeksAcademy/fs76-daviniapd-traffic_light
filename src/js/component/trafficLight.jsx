94413dda22c7dc5413fea64241f91b4fb6211d15
Davinia pd <delgadavi@gmail.com>
2024-08-07 18:30:33 +0000

    creado el componente TrafficLight

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TrafficLight = () => {
+
+    return (
+        <>
+            <div className="col-4 text-center m-5">
+                <h1 style={{ backgroundColor: "red" }}>red</h1>
+                <h1 style={{ backgroundColor: "yellow" }}>ambar</h1>
+                <h1 style={{ backgroundColor: "green" }}>green</h1>
+            </div>
+        </>
+    );
+};
+
+export default TrafficLight;
